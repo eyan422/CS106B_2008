d65b42a7a29baa55eec1a8ee54bdf96562d70316
Frank Yan <eyan422eyan422@gmail.com>
2017-05-27 00:55:49 +0800

    Lecture 4 sample code

---
+++ Lecture_4/Lecture_4_Sample_Code/src/hello.cpp
@@ -0,0 +1,13 @@
+#include "hello.h"
+using namespace std;
+
+int main()
+{
+
+    //CountTokens();
+
+    Vector<int> numbers = MakeRandomVector(5);
+    PrintVector(numbers);
+
+    return 0;
+}
---
+++ Lecture_4/Lecture_4_Sample_Code/src/hello.h
@@ -0,0 +1,46 @@
+// This header file doesn't do much, but it's good to demonstrate that it's
+// possible to create and edit header files
+
+#include <string>
+#include <iostream>
+#include "console.h"
+#include "gwindow.h" // for GWindow
+#include "simpio.h"  // for getLine
+#include "vector.h"  // for Vector
+#include "tokenscanner.h"
+#include <fstream>
+#include "random.h"
+
+using namespace std;
+
+void CountTokens()
+{
+    string str;
+    ifstream infile;
+    TokenScanner scanner;
+
+    cout << "Please enter a sentence: ";
+    cin >> str;
+    scanner.setInput(str);
+
+    int count = 0;
+    while (scanner.hasMoreTokens()) {
+        scanner.nextToken();
+        count++;
+    }
+    cout << "You entered " << count << " tokens." << endl;
+}
+
+Vector<int> MakeRandomVector(int sz)
+{
+    Vector<int> numbers;
+    for (int i = 0; i < sz; i++)
+    numbers.add(randomInteger(1, 100));
+    return numbers;
+}
+
+void PrintVector(Vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    cout << v[i] << " ";
+}
